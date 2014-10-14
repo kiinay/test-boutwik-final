4e0265794fab8facf06e464d4c157b91515be52b
Yannick PLACERDAT <yannick.placerdat@gmail.com>
2014-10-14 12:50:41 +0200

    Base : OK!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestBoutwikFinal::Application.config.secret_token = '4f2651ed6bd2a8989922c3d922a2c3cd661ab8954276dfe9bc18eb5dd3acd6b9a7e56b6a0b16b76a5b4b1c52f426276a4e9e139b887e94ad0649d9258e84b14c'
